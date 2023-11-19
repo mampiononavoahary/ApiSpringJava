be5da1570f76597d948b8de536331c723845371d
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-11-19 15:51:31 +0300

    feat: requete to create table Subscribers

---
+++ src/SQLImplementations/TableSubscribers.sql
@@ -0,0 +1,9 @@
+# CREATE TABLE Subscribers(
+# id varchar(200) primary key not null,
+# name varchar(200),
+# sex char(1),
+# reference varchar(200)
+#);
+
+# INSERT INTO Subscribers values('1','Albert','H','SD124');
+# INSERT INTO Subscribers values('2','EL OAH','H','SD135');
